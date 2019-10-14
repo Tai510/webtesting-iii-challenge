ed8a4933c87b687291271a654090ce3b42b1d64e
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-10-14 14:29:50 -0700

    mvp

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from 'react';
+import { render , cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Controls from '../controls/Controls';
+import Display from '../display/Display';
+afterEach(cleanup)
+
+it('render without crashing', () => {
+    render(<Controls />)
+    render(<Display />)
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,31 @@
 // Test away!
+import React from 'react';
+import { render , fireEvent, findByTestId } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+
+
+it('render without crashing', () => {
+    render(<Display />)
+});
+
+describe('display function tests', () => {
+    test('displays if gate is open/closed and if it is locked/unlocked', () => {
+        const { getByText } = render(<Display locked={true} closed={true} />);
+        getByText('Locked');
+        getByText('Closed');
+    });
+    
+    test('displays if gate is open/closed and if it is locked/unlocked', () => {
+        const { getByText } = render(<Display locked={false} closed={false} />);
+        getByText('Open');
+        getByText('Unlocked');
+    });
+ 
+
+    test('display if closed props is true and open', () => {
+        const { getByText } = render(<Display closed={true} />);
+        getByText('Closed');
+        getByText('Open');
+    })
+})
